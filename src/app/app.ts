b6f22cd864e117bbe7f51c5ceaf75bbf4f387b1a
Aya Ahmed <ayabelal906@gmail.com>
2026-06-04 11:13:46 +0300

    Initial  Angular Khedmetak Front

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterCom } from "./Components/footer-com/footer-com";
+import { NavbarCom } from "./Components/navbar-com/navbar-com";
+import { HomePage } from './Pages/home-page/home-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterCom, NavbarCom,HomePage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
