5cd006a9ed9d7b11e4005a482161502e04944d22
Naglaa <Nagla Fathi@DESKTOP-H4BH2FL>
2026-06-07 21:35:07 +0300

    Loin and signup pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterCom } from "./Components/footer-com/footer-com";
-import { NavbarCom } from "./Components/navbar-com/navbar-com";
+import { FooterCom } from "./Components/SharedComponents/footer-com/footer-com";
+import { NavbarCom } from "./Components/SharedComponents/navbar-com/navbar-com";
 import { HomePage } from './Pages/home-page/home-page';
+import { SignupComponent } from './Pages/signup-page/signup-page';
+import { LoginComponent } from './Pages/login-page/login-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterCom, NavbarCom,HomePage],
+  imports: [ LoginComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
