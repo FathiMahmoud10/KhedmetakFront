b372f1c139030ce2b1a80caceb22c39fbff293e0
Naglaa <Nagla Fathi@DESKTOP-H4BH2FL>
2026-06-20 23:50:39 +0300

    feat: enhance manage services page with CRUD operations and loading states

--- src/app/APIServices/SharedServices/category-api.ts
+++ src/app/APIServices/SharedServices/category-api.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { CategoryDto } from '../../Utilities/Interfaces/ICategory';
 import { HttpClient } from '@angular/common/http';
+import { ApiResponse } from '../../Utilities/Interfaces/IService';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,10 @@ export class CategoryAPI {
 
   constructor(private http: HttpClient) {}
 
+  // الباك إند بيرجع البيانات ملفوفة جوه ApiResponse<IEnumerable<CategoryDto>>
   getAllCategories(): Observable<CategoryDto[]> {
-    return this.http.get<CategoryDto[]>(this.apiUrl);
+    return this.http
+      .get<ApiResponse<CategoryDto[]>>(this.apiUrl)
+      .pipe(map(res => res.data));
   }
 }
